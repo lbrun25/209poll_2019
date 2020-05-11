7d530cbd46b3fc6ee35361edccddaa970ba813f4
Lucien Brun <lucien.brun@epitech.eu>
2020-05-11 16:21:57 +0200

    Add variance Add confidence intervals

--- src/poll/maths.go
+++ src/poll/maths.go
@@ -1,9 +1,38 @@
 package poll
 
-func getPopulationSize() {
+import (
+	"math"
+)
 
+func getVariance() float64 {
+	p := a.p / 100
+	sSize := float64(a.sSize)
+	pSize := float64(a.pSize)
+
+	lhs := p * (1 - p) / sSize
+	rhs := (pSize - sSize) / (pSize - 1)
+	res := lhs * rhs
+	return res
 }
 
-func computeConfidentInterval(value int) {
+func clamp(value float64) float64 {
+	min := 0.0
+	max := 100.0
 
+	if value > max {
+		return max
+	}
+	if value < min {
+		return min
+	}
+	return value
+}
+
+func getConfidentInterval(i float64, variance float64) ConfidenceInterval {
+	interval := (2 * i * math.Sqrt(variance)) / 2 * 100
+
+	return ConfidenceInterval{
+		a: clamp(a.p - interval),
+		b: clamp(a.p + interval),
+	}
 }
--- src/poll/poll.go
+++ src/poll/poll.go
@@ -21,18 +21,25 @@ type PStruct struct {
 }
 
 func displayResults() {
-	fmt.Printf("Population size:                   %d\n", p.populationSize)
-	fmt.Printf("Sample size:                       %d\n", p.sampleSize)
-	fmt.Printf("Voting intentions:                 %.2f%%\n", p.votingIntentions)
-	fmt.Printf("Variance:                          %.6f\n", p.variance)
-	fmt.Printf("95%% confidence interval:           [%.2f%%; %.2f%%]\n", p.confidenceInterval95.a, p.confidenceInterval95.b)
-	fmt.Printf("99%% confidence interval:           [%.2f%%; %.2f%%]\n", p.confidenceInterval99.a, p.confidenceInterval99.b)
+	fmt.Printf("Population size:         %d\n", p.populationSize)
+	fmt.Printf("Sample size:             %d\n", p.sampleSize)
+	fmt.Printf("Voting intentions:       %.2f%%\n", p.votingIntentions)
+	fmt.Printf("Variance:                %.6f\n", p.variance)
+	fmt.Printf("95%% confidence interval: [%.2f%%; %.2f%%]\n", p.confidenceInterval95.a, p.confidenceInterval95.b)
+	fmt.Printf("99%% confidence interval: [%.2f%%; %.2f%%]\n", p.confidenceInterval99.a, p.confidenceInterval99.b)
 }
 
 // Poll - main
 func Poll() {
-	computeConfidentInterval(95)
-	computeConfidentInterval(99)
+	variance := getVariance()
+	p = PStruct{
+		populationSize:       a.pSize,
+		sampleSize:           a.sSize,
+		votingIntentions:     a.p,
+		variance:             variance,
+		confidenceInterval95:  getConfidentInterval(1.96, variance),
+		confidenceInterval99:  getConfidentInterval(2.58, variance),
+	}
 
 	displayResults()
 }
